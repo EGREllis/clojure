2cb1bde1d09da465ac25715da36da39e9e241447
Edward Ellis <edwardellis.egre@gmail.com>
2026-09-02 22:00:02 +0100

    Used loop recur to request pin until valid pin provided.  Does not sanitise input (crashes if not an int).

--- atm/src/atm/core.clj
+++ atm/src/atm/core.clj
@@ -12,6 +12,19 @@
   (let [_ (println prompt-text)]
     (read-line)))
 
+(defn account-for-pin [pin]
+  (first (s/select (fn [acc] (== (:pin acc) (Integer/parseInt pin)))
+            accounts)))
+
+(defn cli-get-account [pin-prompt]
+  (loop [prompt pin-prompt]
+    (let [user-pin (cli-prompt pin-prompt)
+          acc (account-for-pin user-pin)]
+      (if (= nil (:pin acc))
+          (recur prompt)
+          acc)
+    )))
+
 (defn cli-menu-option [menu-text menu-regex]
   (loop [text menu-text
          regex menu-regex]
@@ -23,14 +36,9 @@
         matcher
         ))))
 
-(defn account-for-pin [pin]
-  (s/select (fn [acc] (== (:pin acc) (Integer/parseInt pin)))
-            accounts))
-
 (defn -main
   [& args]
-  (let [pin (cli-prompt "Enter your pin: ")
-        account (first (account-for-pin pin))]
+  (let [account (cli-get-account "Please enter your pin: ")]
     (print (cli-menu-option "Please enter an option between 1-4:\n\t1) Get balance\n\t2) Withdraw\n\t3) Deposit\n\t4) Exit\n" #"[1234]"))
   ))
 
