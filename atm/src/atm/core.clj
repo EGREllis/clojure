ef956cb4dbe6513accfa37a69c5a1cc03bcf323e
Edward Ellis <edwardellis.egre@gmail.com>
2026-09-13 20:58:02 +0100

    Accounts are now updating, however the withdraw is the wrong way around. Prompt for amount has loop without recur.

--- atm/src/atm/core.clj
+++ atm/src/atm/core.clj
@@ -1,25 +1,18 @@
 (ns atm.core
   "Documentation text here"
-  (:require [clojure.set :as s])
-  (:import (java.util.regex Pattern)))
-
-(def accounts #{
-  {:first-name "Ashraf" :balance 1000 :pin 1234}
-  {:first-name "Alex"   :balance 2000 :pin 4321}
-  {:first-name "Edward" :balance 0    :pin 2332}})
+  (:require [clojure.set :as s]))
 
 (defn cli-prompt [prompt-text] 
   (let [_ (println prompt-text)]
     (read-line)))
 
-(defn account-for-pin [pin]
-  (first (s/select (fn [acc] (== (:pin acc) (Integer/parseInt pin)))
-            accounts)))
+(defn account-for-pin [accounts pin]
+  (first (s/select (fn [acc] (== (:pin acc) (Integer/parseInt pin))) accounts)))
 
-(defn cli-get-account [pin-prompt]
+(defn cli-get-account [accounts pin-prompt]
   (loop [prompt pin-prompt]
     (let [user-pin (cli-prompt pin-prompt)
-          acc (account-for-pin user-pin)]
+          acc (account-for-pin accounts user-pin)]
       (if (= nil (:pin acc))
           (recur prompt)
           acc)
@@ -33,12 +26,32 @@
          matcher (re-matches regex input-value)]
       (if (= nil (first matcher))
         (recur text regex)
-        matcher
+        (Integer/parseInt matcher)
         ))))
+
+(defn cli-prompt-for-amount [prompt]
+  (loop [text prompt]
+    (let [user-amount (cli-prompt text)
+          matcher (re-matches #"[\d]+" user-amount)]
+      (Integer/parseInt matcher))
+    ))
+
+;; can not change pin
+(defn update-accounts [accounts new-entry]
+  (let [current-entry (account-for-pin accounts (str (:pin new-entry)))]
+    (disj (conj accounts new-entry) current-entry))
+  )
 
 (defn -main
   [& args]
-  (let [account (cli-get-account "Please enter your pin: ")]
-    (print (cli-menu-option "Please enter an option between 1-4:\n\t1) Get balance\n\t2) Withdraw\n\t3) Deposit\n\t4) Exit\n" #"[1234]"))
-  ))
+  (loop [accounts #{{:first-name "Ashraf" :balance 1000 :pin 1234}
+                    {:first-name "Alex"   :balance 2000 :pin 4321}
+                    {:first-name "Edward" :balance 0    :pin 2332}}]
+    (let [account (cli-get-account accounts "Please enter your pin: ")
+        option (cli-menu-option "Please enter an option between 1-4:\n\t1) Get balance\n\t2) Withdraw\n\t3) Deposit\n\t4) Exit\n" #"[1234]")]
+      (cond (== option 1) (print (str (:first-name account) " your balance is " (:balance account)))
+            (== option 2) (recur (update-accounts accounts (update account :balance (partial - (cli-prompt-for-amount "Please enter the amount you wish to withdraw: ")))))
+            (== option 3) (recur (update-accounts accounts (update account :balance (partial + (cli-prompt-for-amount "Please enter the amount you wish to deposit: ")))))
+            (== option 4) (System/exit 0))
+    )))
 
